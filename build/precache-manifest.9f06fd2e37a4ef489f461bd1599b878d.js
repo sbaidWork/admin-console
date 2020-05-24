aa767d01eb3e6fa78d6925506913c0d97def4273
sbaidWork <dark12arrow@gmail.com>
2020-05-24 12:36:16 +0530

    first commit

---
+++ build/precache-manifest.9f06fd2e37a4ef489f461bd1599b878d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ca528ca6afeef21285ba3f4c50a3b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3634ca5b02bae998b827",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b41bf2b2bfcb1622f31",
+    "url": "/static/js/2.f3d164f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3d164f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3634ca5b02bae998b827",
+    "url": "/static/js/main.6be3a8b4.chunk.js"
+  },
+  {
+    "revision": "c0dcb403cf9c23ff1d8e",
+    "url": "/static/js/runtime-main.0dcfd0b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
